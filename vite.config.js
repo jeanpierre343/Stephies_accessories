6509f588e7045319ea0d690595a727440752f4e5
jeanpierre818 <jeanpierresoueid12@gmail.com>
2026-08-13 15:42:31 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cloudflare(),
+  ],
+
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:4000',
