be7902ad463a932f5fa4cf130455bfd45f6f4dcf
jeanpierre818 <jeanpierresoueid12@gmail.com>
2026-08-13 11:09:11 +0300

    change port listen ip

--- server.js
+++ server.js
@@ -331,6 +331,6 @@ app.post('/api/admin/login', async (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`API server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`API server running on port ${PORT}`);
 });
